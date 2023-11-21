58fa10c8e6ccc48abd85cfe73f40511021e01118
kolhe-apurva <apurvakolhe@outlook.com>
2023-11-20 21:57:19 -0500

    first/final commit

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import "dotenv/config";
+import Hello from "./hello.js";
+import Lab5 from "./lab5.js";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
+import AssignmentRoutes from "./assignments/routes.js";
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.use(express.json());
+CourseRoutes(app);
+ModuleRoutes(app);
+AssignmentRoutes(app);
+Hello(app);
+Lab5(app);
+app.listen(process.env.PORT || 4000);
